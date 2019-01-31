9deb48d23368b804653761e8c3b85c3ee59361f5
Emily Wagner <emilywagner598@gmail.com>
2019-01-31 20:50:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,8 +20,3 @@ class Artist
     song
   end
 end
-
-
-#[#<Song:0x00000001fd5bf0 @name="Ninety Nine Problems", @artist=#<Artist:0x00000001fd5df8 @name="Jay-Z", @songs=[#<Song:0x00000001fd5bf0 ...>]>, @genre=#<Genre:0x00000001fd5d08 @name="rap", @artists=[#<Artist:0x00000001fd5df8 @name="Jay-Z", @songs=[#<Song:0x00000001fd5bf0 ...>]>], @songs=[#<Song:0x00000001fd5bf0 ...>]>>]
-
-#[#<Song:0x00000001fd5bf0 @name="Ninety Nine Problems", @artist=#<Artist:0x00000001fd5df8 @name="Jay-Z", @songs=[#<Song:0x00000001fd5bf0 ...>]>, @genre=#<Genre:0x00000001fd5d08 @name="rap", @artists=[#<Artist:0x00000001fd5df8 @name="Jay-Z", @songs=[#<Song:0x00000001fd5bf0 ...>]>], @songs=[#<Song:0x00000001fd5bf0 ...>]>>]
