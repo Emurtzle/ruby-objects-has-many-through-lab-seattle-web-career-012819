a7d73d5eeb59509ec4a714e1b9ebccaa0970cca5
Emily Wagner <emilywagner598@gmail.com>
2019-01-31 20:24:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,19 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
     @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def new_song(title, genre)
+    song = Song.new(title, self, genre)
+    @songs << song
   end
 end
